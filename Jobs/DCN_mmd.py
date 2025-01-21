f2093a6066c3c3cdceabb03a1b706bc9ebb655cd
Pulkit Thukral <85827394+PulkitT01@users.noreply.github.com>
2025-01-21 15:48:27 +0100

    Include the specialized dropout logic for training vs. evaluation.

--- Jobs/DCN_mmd.py
+++ Jobs/DCN_mmd.py
@@ -1,31 +1,6 @@
-"""
-MIT License
-
-Copyright (c) 2020 Shantanu Ghosh
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils.data
 
 from Utils import Utils
 
@@ -35,14 +10,14 @@ class DCN(nn.Module):
         super(DCN, self).__init__()
         self.training = training_flag
 
-        # shared layer
+        # Shared layers
         self.shared1 = nn.Linear(in_features=input_nodes, out_features=200)
         nn.init.xavier_uniform_(self.shared1.weight)
 
         self.shared2 = nn.Linear(in_features=200, out_features=200)
         nn.init.xavier_uniform_(self.shared2.weight)
 
-        # potential outcome1 Y(1)
+        # Potential outcome Y(1)
         self.hidden1_Y1 = nn.Linear(in_features=200, out_features=200)
         nn.init.xavier_uniform_(self.hidden1_Y1.weight)
 
@@ -52,7 +27,7 @@ class DCN(nn.Module):
         self.out_Y1 = nn.Linear(in_features=200, out_features=2)
         nn.init.xavier_uniform_(self.out_Y1.weight)
 
-        # potential outcome1 Y(0)
+        # Potential outcome Y(0)
         self.hidden1_Y0 = nn.Linear(in_features=200, out_features=200)
         nn.init.xavier_uniform_(self.hidden1_Y0.weight)
 
@@ -66,30 +41,32 @@ class DCN(nn.Module):
         """
         Returns the shared representation (dimension=200) after passing through 
         the shared layers, before branching into Y(1)/Y(0) towers.
-        We mimic the same 'train' vs. 'eval' logic to stay consistent with dropout.
         """
-        from Utils import Utils  # if not already imported at the top
-
         if torch.cuda.is_available():
             x = x.float().cuda()
         else:
             x = x.float()
 
         if self.training:
-            # Exactly what __train_net does for the shared layers:
+            # In 'training' mode, we do specialized dropout logic:
             entropy = Utils.get_shanon_entropy(ps_score.item())
             dropout_prob = Utils.get_dropout_probability(entropy, gama=1)
             shared_mask = Utils.get_dropout_mask(dropout_prob, self.shared1(x))
+
             x = F.relu(shared_mask * self.shared1(x))
             x = F.relu(shared_mask * self.shared2(x))
         else:
-            # Exactly what __eval_net does for the shared layers:
+            # In 'eval' mode, no custom dropout:
             x = F.relu(self.shared1(x))
             x = F.relu(self.shared2(x))
 
         return x
 
     def forward(self, x, ps_score):
+        """
+        Forward returns (logits_y1, logits_y0).
+        We branch into either __train_net (with custom dropout) or __eval_net.
+        """
         if torch.cuda.is_available():
             x = x.float().cuda()
         else:
@@ -103,21 +80,22 @@ class DCN(nn.Module):
         return y1, y0
 
     def __train_net(self, x, ps_score):
+        # Specialized dropout logic for the "shared" layers
         entropy = Utils.get_shanon_entropy(ps_score.item())
         dropout_prob = Utils.get_dropout_probability(entropy, gama=1)
 
-        # shared layers
+        # Shared layers
         shared_mask = Utils.get_dropout_mask(dropout_prob, self.shared1(x))
         x = F.relu(shared_mask * self.shared1(x))
         x = F.relu(shared_mask * self.shared2(x))
 
-        # potential outcome1 Y(1)
+        # Potential outcome Y(1)
         y1_mask = Utils.get_dropout_mask(dropout_prob, self.hidden1_Y1(x))
         y1 = F.relu(y1_mask * self.hidden1_Y1(x))
         y1 = F.relu(y1_mask * self.hidden2_Y1(y1))
         y1 = self.out_Y1(y1)
 
-        # potential outcome1 Y(0)
+        # Potential outcome Y(0)
         y0_mask = Utils.get_dropout_mask(dropout_prob, self.hidden1_Y0(x))
         y0 = F.relu(y0_mask * self.hidden1_Y0(x))
         y0 = F.relu(y0_mask * self.hidden2_Y0(y0))
@@ -126,16 +104,16 @@ class DCN(nn.Module):
         return y1, y0
 
     def __eval_net(self, x):
-        # shared layers
+        # Standard forward pass (no special dropout)
         x = F.relu(self.shared1(x))
         x = F.relu(self.shared2(x))
 
-        # potential outcome1 Y(1)
+        # Y(1)
         y1 = F.relu(self.hidden1_Y1(x))
         y1 = F.relu(self.hidden2_Y1(y1))
         y1 = self.out_Y1(y1)
 
-        # potential outcome1 Y(0)
+        # Y(0)
         y0 = F.relu(self.hidden1_Y0(x))
         y0 = F.relu(self.hidden2_Y0(y0))
         y0 = self.out_Y0(y0)
